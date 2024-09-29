4eb7eb9c6cdcfd6e857aedb0c617ee4c465f3d48
VladGGDev <99538507+VladGGDev@users.noreply.github.com>
2024-09-29 14:55:33 +0300

    Reworked hero and started work on cards
    The hero section now has a scrolling background gradient and random splash texts.
    
    I have laid the base for cards for displaying links of my games  and web experiments.

--- script.js
+++ script.js
@@ -1,15 +1,134 @@
-function ScrollDown()
+// Utility functions
+function RandomBetween(a, b)
 {
-	scrollBy(0, screen.availHeight);
+	return a + Math.floor(Math.random() * (b - a));
 }
 
-document.getElementById("about-me-link").addEventListener("click", ScrollDown);
-document.getElementById("my-games-link").addEventListener("click", ScrollDown);
-document.getElementById("my-web-experiments-link").addEventListener("click", ScrollDown);
+function RandomFromCollection(collection)
+{
+	return collection[RandomBetween(0, collection.length)];
+}
+
 
 
 // Age
 const year = 31536000000;  // 1000ms * 60s * 60m * 24h * 365d
-const birthday = 14515200000;  // 1000ms * 60s * 60m * 24h* 168d = time from 1 january to 15 june
-let myAge = Math.round((Date.now() - birthday) / year) - 37;
+const myBirthday = new Date("2007-06-15T12:00:00");
+const myAge = Math.floor((Date.now() - myBirthday) / year);
 document.getElementById("age").innerText = myAge;
+
+
+
+// ===== Hero =====
+// Hero background scrolling
+const hero = document.getElementById("hero");
+let bgOffset = 0;
+setInterval(() =>
+{
+	hero.style.backgroundPositionX = bgOffset + "%";
+	bgOffset += 0.05;
+}, 10);
+
+
+// Hero splash text
+// Splash texts can be:
+// - strings => returned if selected
+// - arrays with the format [text, display chance, isItalic]
+const splashTexts = [
+	"Lorem ipsum dolor sit amet",
+	["S... Skibidi? (I'm sorry)", 0.1],
+	"This is some splash text!",
+	"Anyone can code",
+	"Web development is easy",
+	'Web development is "easy"',
+	["💪💪💪", 0.5, false],
+	"Probably reading some API documentation rn",
+	"Cooking something fresh",
+	"!?!?!?!?!?!?",
+	"!!!!!!!!!!!!",
+	"Refresh this page for new splash text",
+	"Who doesn't love video games?",
+	["Celeste is one of my favourite games", 0.75],
+	["Hollow Knight is one of my favourite games", 0.75],
+	["How could you tell I watch anime?", 0.75],
+	["Are these splash texts a little narcissistic?", 0.25],
+	"Professionalism!",
+	"To be or not to be?",
+	"Hmmmmm...",
+	["As a matter of fact, I am pushing P.<br>That's pushing powder down my musket so I may smite another opp!<br>😈😈😈", 0.1, false],
+	["🤓", 0.5, false],
+	["😎😎😎", 0.8, false],
+	['These were coded so I could use &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp <span style="display: inline-block; transform: scale(2) perspective(50px) rotateX(34deg) ">HTML and CSS</span> &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp in splash texts', 1, false],
+	"This one's a little <b>bold</b>",
+	"Don't stop reading books",
+	["1 million beers!", 0.5],
+	"Narwhals are real animals",
+	["You have just lost<b>The Game</b>", 0.5],
+	"Ice cream time!",
+	"Don't burn yourself out",
+	"Kebab is my favourite food!!",
+	["E = mc<sup>2</sup>", 1, false],
+	"Minecraft was revolutionary when it came out",
+	"Hello world!",
+	"Don't forget to drink water!",
+	"I'm also a game developer",
+	"I'm alsa a web developer",
+	"I can also do pixel art",
+	"I know Blender somewhat",
+	"Quality over quantity",
+	["Sometimes I play video games instead of programming 😏", 0.5, false],
+	["😏😏😏", 0.5, false],
+	'Say "no!" to bugs',
+	"Bugs are my worst enemy >:(",
+];
+
+
+const splash = document.getElementById("splash-text");
+
+// Used for debug
+function ForceSplash(text)
+{
+	if (typeof text === "string")
+	{
+		splash.innerHTML = text;
+	}
+	else
+	{
+		// Is an array with commands
+		if (text[2] === false)
+		{
+			splash.style.fontStyle = "normal";
+			console.log("Set font style " + text[0] + " to normal");
+		}
+		splash.innerHTML = text[0];
+		console.log("Splash text is " + text[0]);
+	}
+}
+
+function ChangeSplash()
+{
+	let selectedFromCollection;
+	// Keep selecting if the selection is an object and the chance check doesn't work
+	selectedFromCollection = RandomFromCollection(splashTexts);
+
+	if (typeof selectedFromCollection === "string")
+	{
+		splash.innerHTML = selectedFromCollection;
+	}
+	else
+	{
+		// Is an array with commands
+		if (selectedFromCollection[1] <= Math.random())
+		{
+			ChangeSplash();
+			return;
+		}
+		if (selectedFromCollection[2] === false)
+		{
+			splash.style.fontStyle = "normal";
+		}
+		splash.innerHTML = selectedFromCollection[0];
+	}
+}
+
+ChangeSplash();
