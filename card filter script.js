62c48914faa97d7aaa311554aa51302641781cc5
VladGGDev <99538507+VladGGDev@users.noreply.github.com>
2024-10-07 21:46:45 +0300

    Finished website layout
    Now I only need to add content. I have also added a disclaimer at the bottom of the hero section stating that the website is still a work in progress.

--- card filter script.js
+++ card filter script.js
@@ -1,16 +1,40 @@
+const cards = document.querySelectorAll(".card");
+function DisplayCardsWithTag(tag)
+{
+	for (let i = 0; i < cards.length; i++)
+	{
+		if (cards[i].getAttribute("data-filter-tag").includes(tag))
+		{
+			cards[i].style.display = "block";
+		}
+		else
+		{
+			cards[i].style.display = "none";
+		}
+	}
+}
+
+const filters = document.querySelectorAll(".filter");
 const filterContainer = document.querySelector(".filter-container");
+function SelectFilter(filter)
+{
+	filterContainer.style.setProperty(
+		"--selection-width",
+		`${filter.offsetWidth}px`
+	);
+	filterContainer.style.setProperty(
+		"--selection-offset-x",
+		`${filter.offsetLeft}px`
+	);
+	DisplayCardsWithTag(filter.getAttribute("data-filter"));
+}
+
 filterContainer.addEventListener("mousedown", (event) =>
 {
 	if (event.target.classList.contains("filter"))
 	{
-		filterContainer.style.setProperty(
-			"--selection-width",
-			`${event.target.offsetWidth}px`
-		);
-		filterContainer.style.setProperty(
-			"--selection-offset-x",
-			`${event.target.offsetLeft}px`
-		);
-		console.log("width: " + event.target.offsetWidth + "\nx pos: " + event.target.offsetLeft);
+		SelectFilter(event.target);
 	}
 });
+
+SelectFilter(filters[0]);
